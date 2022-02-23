900ca246abcf1f08e0cfff333619f0eb61ea3f81
khang <khang220300@gmail.com>
2022-02-23 15:11:19 +0700

    update: multi languages

--- lib/modules/walk_through/controller/walk_through_controller.dart
+++ lib/modules/walk_through/controller/walk_through_controller.dart
@@ -18,6 +18,8 @@ class WakThroughController extends GetxController {
   var currentIndexWalkThrough = 0.obs;
   var authService = Get.find<AuthService>();
 
+  var selectedLanguage = "VN".obs;
+
   Timer? timer;
 
   @override
@@ -28,141 +30,8 @@ class WakThroughController extends GetxController {
   @override
   void onInit() {
     super.onInit();
-    initPage();
     checkLogin();
     timer = Timer.periodic(const Duration(seconds: 3), (Timer t) => autoLoop());
-  }
-
-  initPage() {
-    pages = [
-      Column(
-        crossAxisAlignment: CrossAxisAlignment.center,
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Image.asset(
-            "assets/images/walkthrough1.png",
-            height: 250,
-          ),
-          40.height,
-          SizedBox(
-            width: 300,
-            child: Text(
-              "title_walk_through_1".tr,
-              style: boldTextStyle(
-                color: textColorPrimary,
-                size: 20,
-                letterSpacing: 1,
-              ),
-              textAlign: TextAlign.center,
-            ),
-          ),
-          const SizedBox(
-            height: 5,
-          ),
-          SizedBox(
-            width: 340,
-            child: Text(
-              "description_walk_through_1".tr,
-              style:
-                  primaryTextStyle(color: textColorPrimary, letterSpacing: 1),
-              textAlign: TextAlign.center,
-            ),
-          ),
-          // commonCacheImageWidget(tc_WalkThroughImg1.validate(), height: 230),
-        ],
-      ),
-      Column(
-        crossAxisAlignment: CrossAxisAlignment.center,
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Image.asset(
-            "assets/images/walkthrough2.png",
-            height: 250,
-          ),
-          40.height,
-          SizedBox(
-            width: 250,
-            child: Text(
-              "title_walk_through_2".tr,
-              style: boldTextStyle(
-                  color: textColorPrimary, size: 20, letterSpacing: 1),
-              textAlign: TextAlign.center,
-            ),
-          ),
-          const SizedBox(
-            height: 5,
-          ),
-          SizedBox(
-            width: 240,
-            child: Text(
-              "description_walk_through_2".tr,
-              style:
-                  primaryTextStyle(color: textColorPrimary, letterSpacing: 1),
-              textAlign: TextAlign.center,
-            ),
-          ),
-        ],
-      ),
-      Column(
-        crossAxisAlignment: CrossAxisAlignment.center,
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Image.asset(
-            "assets/images/walkthrough3.png",
-            height: 250,
-          ),
-          40.height,
-          Text(
-            "title_walk_through_3".tr,
-            style: boldTextStyle(
-                color: textColorPrimary, size: 20, letterSpacing: 1),
-          ),
-          const SizedBox(
-            height: 5,
-          ),
-          SizedBox(
-              width: 260,
-              child: Text(
-                "description_walk_through_3".tr,
-                style: primaryTextStyle(
-                  color: textColorPrimary,
-                  letterSpacing: 1,
-                ),
-                textAlign: TextAlign.center,
-              )),
-        ],
-      ),
-      Column(
-        crossAxisAlignment: CrossAxisAlignment.center,
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Image.asset(
-            "assets/images/walkthrough4.png",
-            height: 250,
-          ),
-          40.height,
-          Text(
-            "title_walk_through_4".tr,
-            style: boldTextStyle(
-                color: textColorPrimary, size: 20, letterSpacing: 1),
-          ),
-          const SizedBox(
-            height: 5,
-          ),
-          SizedBox(
-            width: 260,
-            child: Text(
-              "description_walk_through_4".tr,
-              style: primaryTextStyle(
-                color: textColorPrimary,
-                letterSpacing: 1,
-              ),
-              textAlign: TextAlign.center,
-            ),
-          ),
-        ],
-      ),
-    ];
   }
 
   Future<User?> signInByGoogleAccount() async {
--- lib/modules/walk_through/view/walk_through_page.dart
+++ lib/modules/walk_through/view/walk_through_page.dart
@@ -3,6 +3,7 @@ import 'package:flutter_easyloading/flutter_easyloading.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:get/get.dart';
 import 'package:nb_utils/nb_utils.dart';
+import 'package:smart_dropdown/smart_dropdown.dart';
 import 'package:trellis_mobile_app/modules/walk_through/controller/walk_through_controller.dart';
 import 'package:trellis_mobile_app/modules/walk_through/view/widget.dart';
 import 'package:trellis_mobile_app/routes/app_routes.dart';
@@ -14,159 +15,225 @@ import 'package:flag/flag.dart';
 class WalkThroughPage extends StatelessWidget {
   WalkThroughPage({Key? key}) : super(key: key);
   final walkThroughController = Get.find<WakThroughController>();
+  List<Widget> pages = [];
   @override
   Widget build(BuildContext context) {
     return SafeArea(
-        child: Scaffold(
-      backgroundColor: backgroundColor,
-      body: SingleChildScrollView(
-        padding: EdgeInsets.all(16.w),
-        child: Column(
-          children: [
-            Stack(
-              children: [
-                Positioned(
-                  left: Get.width / 2 - 270.w,
-                  child: Text(
-                    TCAppName,
-                    style: TextStyle(
-                        color: Colors.white,
-                        fontSize: 200.sp,
-                        letterSpacing: 2),
-                  ),
-                  top: 50.h,
-                ),
-                50.height,
-                Column(
-                  crossAxisAlignment: CrossAxisAlignment.center,
-                  children: [
-                    SizedBox(
-                      height: Get.height * .75,
-                      child: PageView(
-                        controller: walkThroughController.pageController,
-                        scrollDirection: Axis.horizontal,
-                        children: walkThroughController.pages,
-                        onPageChanged: (index) {
-                          walkThroughController.currentIndexWalkThrough.value =
-                              index;
-                        },
-                      ),
-                    ),
-                  ],
-                ),
-                AnimatedPositioned(
-                  duration: const Duration(seconds: 1),
-                  bottom: 20,
-                  left: 0,
-                  right: 0,
-                  child: DotIndicator(
-                      pages: walkThroughController.pages,
-                      indicatorColor: Colors.white,
-                      pageController: walkThroughController.pageController),
-                ),
-              ],
-            ),
-            Container(
-              padding: EdgeInsets.fromLTRB(0, 15.w, 0, 15.w),
-              width: Get.width,
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                mainAxisSize: MainAxisSize.max,
+      child: Scaffold(
+        backgroundColor: backgroundColor,
+        body: SingleChildScrollView(
+          padding: EdgeInsets.all(16.w),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: [
+              30.height,
+              Stack(
                 children: [
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceAround,
-                    children: [
-                      SizedBox(
-                        width: 400.w,
-                        height: 200.h,
-                        child: ElevatedButton(
-                          onPressed: () async {
-                            await openBottomSheetSignUp();
-                          },
-                          style: ElevatedButton.styleFrom(
-                              shape: RoundedRectangleBorder(
-                                borderRadius: BorderRadius.circular(5),
-                              ),
-                              padding: const EdgeInsets.symmetric(
-                                  vertical: 16, horizontal: 20),
-                              primary: buttonColor),
-                          child: Text("sign_up".tr,
-                              style: TextStyle(
-                                  color: textColorPrimary, fontSize: 56.sp)),
-                        ),
-                      ),
-                      SizedBox(
-                        width: 400.w,
-                        height: 200.h,
-                        child: ElevatedButton(
-                          onPressed: () async {
-                            await openBottomSheetSignIn();
-                          },
-                          style: ElevatedButton.styleFrom(
-                              shape: RoundedRectangleBorder(
-                                borderRadius: BorderRadius.circular(5),
-                              ),
-                              padding: const EdgeInsets.symmetric(
-                                  vertical: 16, horizontal: 20),
-                              primary: buttonColor),
-                          child: Text("sign_in".tr,
-                              style: TextStyle(
-                                  color: textColorPrimary, fontSize: 56.sp)),
-                        ),
-                      ),
-                    ],
-                  ),
-                  20.height,
-                  Container(
-                    margin: EdgeInsets.only(left: 10.w, right: 10.w),
-                    child: RichText(
-                      textAlign: TextAlign.center,
-                      text: TextSpan(
-                        children: [
-                          TextSpan(
-                              text: "termsCondition1".tr,
-                              style: secondaryTextStyle(
-                                  color: textColorSecondary,
-                                  size: 56.sp.toInt())),
-                          TextSpan(
-                            text: "termsCondition2".tr,
-                            style: boldTextStyle(
-                              color: textColorPrimary,
-                              size: 56.sp.toInt(),
-                            ),
-                          ),
-                          TextSpan(
-                            text: "termsCondition3".tr,
-                            style: secondaryTextStyle(
-                              color: textColorSecondary,
-                              size: 56.sp.toInt(),
-                            ),
-                          ),
-                          TextSpan(
-                            text: "termsCondition4".tr,
-                            style: boldTextStyle(
-                              color: textColorPrimary,
-                              size: 56.sp.toInt(),
-                            ),
-                          ),
-                          TextSpan(
-                            text: "termsCondition5".tr,
-                            style: secondaryTextStyle(
-                              color: textColorSecondary,
-                              size: 56.sp.toInt(),
-                            ),
-                          ),
-                        ],
-                      ),
+                  Align(
+                    alignment: Alignment.centerLeft,
+                    child: Obx(
+                      () => _buildDropdownLanguage(),
                     ),
-                  )
+                  ),
+                  Align(
+                    alignment: Alignment.topCenter,
+                    child: Text(
+                      TCAppName,
+                      style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 200.sp,
+                          letterSpacing: 2),
+                    ),
+                  ),
                 ],
               ),
-            )
-          ],
+              Column(
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: [
+                  SizedBox(
+                    height: Get.height * .50,
+                    child: PageView(
+                      controller: walkThroughController.pageController,
+                      scrollDirection: Axis.horizontal,
+                      children: initPage(),
+                      onPageChanged: (index) {
+                        walkThroughController.currentIndexWalkThrough.value =
+                            index;
+                      },
+                    ),
+                  ),
+                  30.height,
+                  AnimatedContainer(
+                    duration: const Duration(seconds: 1),
+                    child: DotIndicator(
+                        pages: initPage(),
+                        indicatorColor: Colors.white,
+                        pageController: walkThroughController.pageController),
+                  ),
+                ],
+              ),
+              10.height,
+              Container(
+                padding: EdgeInsets.fromLTRB(0, 15.w, 0, 15.w),
+                width: Get.width,
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  mainAxisSize: MainAxisSize.max,
+                  children: [
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceAround,
+                      children: [
+                        SizedBox(
+                          width: 400.w,
+                          height: 200.h,
+                          child: ElevatedButton(
+                            onPressed: () async {
+                              await openBottomSheetSignUp();
+                            },
+                            style: ElevatedButton.styleFrom(
+                                shape: RoundedRectangleBorder(
+                                  borderRadius: BorderRadius.circular(5),
+                                ),
+                                padding: const EdgeInsets.symmetric(
+                                    vertical: 16, horizontal: 20),
+                                primary: buttonColor),
+                            child: Text("sign_up".tr,
+                                style: TextStyle(
+                                    color: textColorPrimary, fontSize: 56.sp)),
+                          ),
+                        ),
+                        SizedBox(
+                          width: 400.w,
+                          height: 200.h,
+                          child: ElevatedButton(
+                            onPressed: () async {
+                              await openBottomSheetSignIn();
+                            },
+                            style: ElevatedButton.styleFrom(
+                                shape: RoundedRectangleBorder(
+                                  borderRadius: BorderRadius.circular(5),
+                                ),
+                                padding: const EdgeInsets.symmetric(
+                                    vertical: 16, horizontal: 20),
+                                primary: buttonColor),
+                            child: Text("sign_in".tr,
+                                style: TextStyle(
+                                    color: textColorPrimary, fontSize: 56.sp)),
+                          ),
+                        ),
+                      ],
+                    ),
+                    30.height,
+                    Container(
+                      margin: EdgeInsets.only(left: 10.w, right: 10.w),
+                      child: RichText(
+                        textAlign: TextAlign.center,
+                        text: TextSpan(
+                          children: [
+                            TextSpan(
+                                text: "termsCondition1".tr,
+                                style: secondaryTextStyle(
+                                    color: textColorSecondary,
+                                    size: 56.sp.toInt())),
+                            TextSpan(
+                              text: "termsCondition2".tr,
+                              style: boldTextStyle(
+                                color: textColorPrimary,
+                                size: 56.sp.toInt(),
+                              ),
+                            ),
+                            TextSpan(
+                              text: "termsCondition3".tr,
+                              style: secondaryTextStyle(
+                                color: textColorSecondary,
+                                size: 56.sp.toInt(),
+                              ),
+                            ),
+                            TextSpan(
+                              text: "termsCondition4".tr,
+                              style: boldTextStyle(
+                                color: textColorPrimary,
+                                size: 56.sp.toInt(),
+                              ),
+                            ),
+                            TextSpan(
+                              text: "termsCondition5".tr,
+                              style: secondaryTextStyle(
+                                color: textColorSecondary,
+                                size: 56.sp.toInt(),
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                    )
+                  ],
+                ),
+              )
+            ],
+          ),
         ),
       ),
-    ));
+    );
+  }
+
+  Widget _buildDropdownLanguage() {
+    String selectedLanguage = walkThroughController.selectedLanguage.value;
+    return DropdownButton<String>(
+      iconSize: 0,
+      hint: selectedLanguage == "VN"
+          ? Flag.fromCode(
+              FlagsCode.VN,
+              width: 60,
+              height: 30,
+            )
+          : Flag.fromCode(
+              FlagsCode.US,
+              width: 60,
+              height: 30,
+            ),
+      items: [
+        DropdownMenuItem(
+          alignment: Alignment.centerLeft,
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: [
+              Flag.fromCode(
+                FlagsCode.VN,
+                width: 50,
+                height: 25,
+              ),
+              Text("Tiếng Việt"),
+            ],
+          ),
+          value: "VN",
+        ),
+        DropdownMenuItem(
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: [
+              Flag.fromCode(
+                FlagsCode.US,
+                width: 50,
+                height: 25,
+              ),
+              Text("English"),
+            ],
+          ),
+          value: "US",
+        ),
+      ],
+      onChanged: (value) {
+        walkThroughController.selectedLanguage.value = value!;
+        if (value == "US") {
+          var locale = const Locale('en', 'US');
+          Get.updateLocale(locale);
+        } else {
+          var locale = const Locale('vi', 'VN');
+          Get.updateLocale(locale);
+        }
+      },
+    );
   }
 
   Future<dynamic> openBottomSheetSignUp() {
@@ -222,5 +289,137 @@ class WalkThroughPage extends StatelessWidget {
         ],
       ),
     ));
+  }
+
+  initPage() {
+    return pages = [
+      Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Image.asset(
+            "assets/images/walkthrough1.png",
+            height: 250,
+          ),
+          40.height,
+          SizedBox(
+            width: 300,
+            child: Text(
+              "title_walk_through_1".tr,
+              style: boldTextStyle(
+                color: textColorPrimary,
+                size: 20,
+                letterSpacing: 1,
+              ),
+              textAlign: TextAlign.center,
+            ),
+          ),
+          const SizedBox(
+            height: 5,
+          ),
+          SizedBox(
+            width: 340,
+            child: Text(
+              "description_walk_through_1".tr,
+              style:
+                  primaryTextStyle(color: textColorPrimary, letterSpacing: 1),
+              textAlign: TextAlign.center,
+            ),
+          ),
+          // commonCacheImageWidget(tc_WalkThroughImg1.validate(), height: 230),
+        ],
+      ),
+      Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Image.asset(
+            "assets/images/walkthrough2.png",
+            height: 250,
+          ),
+          40.height,
+          SizedBox(
+            width: 250,
+            child: Text(
+              "title_walk_through_2".tr,
+              style: boldTextStyle(
+                  color: textColorPrimary, size: 20, letterSpacing: 1),
+              textAlign: TextAlign.center,
+            ),
+          ),
+          const SizedBox(
+            height: 5,
+          ),
+          SizedBox(
+            width: 240,
+            child: Text(
+              "description_walk_through_2".tr,
+              style:
+                  primaryTextStyle(color: textColorPrimary, letterSpacing: 1),
+              textAlign: TextAlign.center,
+            ),
+          ),
+        ],
+      ),
+      Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Image.asset(
+            "assets/images/walkthrough3.png",
+            height: 250,
+          ),
+          40.height,
+          Text(
+            "title_walk_through_3".tr,
+            style: boldTextStyle(
+                color: textColorPrimary, size: 20, letterSpacing: 1),
+          ),
+          const SizedBox(
+            height: 5,
+          ),
+          SizedBox(
+              width: 260,
+              child: Text(
+                "description_walk_through_3".tr,
+                style: primaryTextStyle(
+                  color: textColorPrimary,
+                  letterSpacing: 1,
+                ),
+                textAlign: TextAlign.center,
+              )),
+        ],
+      ),
+      Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Image.asset(
+            "assets/images/walkthrough4.png",
+            height: 250,
+          ),
+          40.height,
+          Text(
+            "title_walk_through_4".tr,
+            style: boldTextStyle(
+                color: textColorPrimary, size: 20, letterSpacing: 1),
+          ),
+          const SizedBox(
+            height: 5,
+          ),
+          SizedBox(
+            width: 260,
+            child: Text(
+              "description_walk_through_4".tr,
+              style: primaryTextStyle(
+                color: textColorPrimary,
+                letterSpacing: 1,
+              ),
+              textAlign: TextAlign.center,
+            ),
+          ),
+        ],
+      ),
+    ];
   }
 }
